f0fe8fe21840ad7b22378dbd29d162a8cd8117fd
EdisonW <nan@Edison-Wang.local>
2024-07-09 12:18:27 +0800

    update

---
+++ backend/check_inspiration_token.py
@@ -0,0 +1,5 @@
+import requests,json
+url =  "https://kawo.sentry.io/issues/?environment=production&project=215896&query=expired&referrer=issue-list&statsPeriod=24h"
+# headers = {"Authorization": "Bearer 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"}
+res = requests.get(url=url)
+print(res.text)
